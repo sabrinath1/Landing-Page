1cdc55876a4e6a19cb8ea1a4aca3bcf7aad720ba
Sabrina <sabrinath1@hotmail.com>
2023-06-21 14:40:48 -0300

    first commit

---
+++ app.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ptBR, datePtBR } from "naive-ui";
+import { NThemeEditor } from "naive-ui";
+</script>
+<template>
+  <n-theme-editor>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </n-theme-editor>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+</template>
