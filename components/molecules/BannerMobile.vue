770c9744a26ed4e9e6675aedd7ed51c6eeb2628f
Sabrina <sabrinath1@hotmail.com>
2023-06-27 09:41:03 -0300

    banner main version mobile

---
+++ components/molecules/BannerMobile.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const props = defineProps<{
+  imgMobile: string;
+}>();
+</script>
+<template>
+  <div
+    :style="{ backgroundImage: `url(${props.imgMobile})` }"
+    class="hero mb-20 block md:hidden"
+  >
+    <div class="text-end text-neutral-content h-[30rem] w-full px-5 sm:px-20">
+      <div class="flex-col pt-16 w-72 justify-center text-center ml-32">
+        <h1 class="mb-5 pl-4 text-3xl xl:text-7xl font-bold pt-28 text-primary">
+          Sabrina Campos
+        </h1>
+        <h1 class="mb-5 px-6 text-2xl font-bold text-center flex-wrap pt-6">
+          Advocacia e Assessoria Jurídica
+        </h1>
+      </div>
+
+      <AtomsParagraph> </AtomsParagraph>
+      <!-- <div class="sm:py-10 invisible sm:visible">
+        <AtomsButtonContact
+          ghost
+          color="#a38a43"
+          text-color="#ffffff"
+          size="large"
+        >
+          Saber Mais
+        </AtomsButtonContact>
+      </div> -->
+    </div>
+  </div>
+</template>
--- components/organisms/BannerMain.vue
+++ components/organisms/BannerMain.vue
@@ -2,4 +2,5 @@
   <MoleculesBannerDesktop
     imgDesktop="img/banner-main.png"
   ></MoleculesBannerDesktop>
+  <MoleculesBannerMobile imgMobile="img/banner-mobile.png" />
 </template>
